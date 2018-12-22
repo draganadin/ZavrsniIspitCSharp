2cc31812f6d62a3dfb1ecadb7b9e4c88e9f560fb
draganadin <dd.dinicdragana@gmail.com>
2018-12-22 12:40:52 +0100

    Resenja zadataka za zavrsni ispit - C#

---
+++ Zadatak1/Zadatak1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Unesite zeljenu godinu: ");
+            year = Convert.ToInt16(Console.ReadLine());
+
+            if ((year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0)))
+            {
+                Console.WriteLine("Godina {0} je prestupna godina.", year);
+            }
+            else
+            {
+                Console.WriteLine("Godina {0} nije prestupna godina.", year);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Zadatak2/Zadatak2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+
+            do
+            {
+                Console.WriteLine("Unesite željeni broj (između 6 i 10): ");
+                n = Convert.ToInt32(Console.ReadLine());
+            } while (n < 6 || n > 10);
+
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("{0}", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
